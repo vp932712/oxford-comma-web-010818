628c8b9b63d73abb333eb2671030e085e332e8b0
Vaibhav Patel <vp932712@gmail.com>
2017-12-12 21:40:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,18 @@
+
+
 def oxford_comma(array)
 if array.length == 1
   array.join("")
 elsif array.length == 2
   array.join(" and ")
-
+else 
+  empty = []
+i = 0
+while (i < array.length-1)
+  empty.push("#{array[i]},")
+  i += 1
 end
+ 
+end
+empty.join(" ") + " and "+ array[array.length-1]
 end
