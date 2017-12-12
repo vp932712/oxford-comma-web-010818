705524b52d41a8512fa6f3d5bd6413e14f0cc729
Vaibhav Patel <vp932712@gmail.com>
2017-12-12 21:58:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
+
+
+
+
 def oxford_comma(array)
+  empty = []
+if array.length == 1
+  array.join("")
+
+elsif array.length == 2
+  array.join(" and ")
+ else
+
+
+i = 0
+while (i < array.length-1)
+  empty.push("#{array[i]},")
+  i += 1
+end
+empty.join(" ") + " and "+ array[array.length-1]
+
+end
+
 
 end
