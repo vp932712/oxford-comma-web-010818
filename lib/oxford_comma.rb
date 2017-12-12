f2013b63d9aeadedfa7369270ba02376a15c9846
Vaibhav Patel <vp932712@gmail.com>
2017-12-12 21:40:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,14 +5,14 @@ if array.length == 1
   array.join("")
 elsif array.length == 2
   array.join(" and ")
-else 
+else
   empty = []
 i = 0
 while (i < array.length-1)
   empty.push("#{array[i]},")
   i += 1
 end
- 
+
 end
 empty.join(" ") + " and "+ array[array.length-1]
 end
